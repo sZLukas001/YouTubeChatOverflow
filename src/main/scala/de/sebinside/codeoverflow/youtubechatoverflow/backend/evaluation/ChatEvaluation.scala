c7ce1548e588fa981171be64d8c4c4a2c900065f
0xSeb <sebah@gmx.de>
2016-12-04 17:23:18 +0100

    added getMostUsedWord function

--- src/main/scala/de/sebinside/codeoverflow/youtubechatoverflow/backend/evaluation/ChatEvaluation.scala
+++ src/main/scala/de/sebinside/codeoverflow/youtubechatoverflow/backend/evaluation/ChatEvaluation.scala
@@ -18,10 +18,21 @@ public class ChatEvaluation(messageProvider: YouTubeMessageProvider) {
   /**
     * @return all messages sent during the last n milliseconds as provided by the YouTubeMessageProvider
     */
-  def getMessages(lastMilliseconds : Long) = {
+  def getMessages(lastMilliseconds : Long): List[LiveChatMessage] = {
     messageProvider.getMessages(lastMilliseconds)
   }
 
+  def getMostUsedWord(lastMilliseconds : Long): String = {
+    getMessages(lastMilliseconds) //all messages of last n milliseconds
+      .map(msg => msg.getSnippet.getDisplayMessage) //extract text from messages
+      .mkString(" ") //concat messages using " "
+      .split("\\s") //split to single words
+      .groupBy(msg => msg.toLowerCase.replaceAll("\\W", ""))
+      //group by words, map words to lowercase and replace non character signs to find duplicates better
+      .mapValues(array => array.size) //count number of occurences of every word
+      .maxBy(_._2) //find tuple (most used word, number of uses)
+      ._1 //get word from tuple
+  }
 }
 
 object ChatEvaluation {
