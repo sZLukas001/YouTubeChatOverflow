7866a4ef4cfd4254dd65e061a6be372aec14c500
0xSeb <sebah@gmx.de>
2016-12-04 16:46:06 +0100

    added wrapper methods in ChatEvaluation

--- src/main/scala/de/sebinside/codeoverflow/youtubechatoverflow/backend/evaluation/ChatEvaluation.scala
+++ src/main/scala/de/sebinside/codeoverflow/youtubechatoverflow/backend/evaluation/ChatEvaluation.scala
@@ -1,15 +1,31 @@
 package de.sebinside.codeoverflow.youtubechatoverflow.backend.evaluation
 
+import com.google.api.services.youtube.model.LiveChatMessage
 import de.sebinside.codeoverflow.youtubechatoverflow.backend.YouTubeMessageProvider
 
 /**
   * Created by seb on 29.11.2016.
   */
-class ChatEvaluation(messageProvider: YouTubeMessageProvider) {
+public class ChatEvaluation(messageProvider: YouTubeMessageProvider) {
 
-  // TODO: Dennis evaluation stuff
+  /**
+    * @return all messages as provided by the YouTubeMessageProvider
+    */
+  def getMessages : List[LiveChatMessage] = {
+    messageProvider.getMessages
+  }
 
-  // Not package private = Acessible from outside (-> project)
-  def doSomething(): Unit = ???
+  /**
+    * @return all messages sent during the last n milliseconds as provided by the YouTubeMessageProvider
+    */
+  def getMessages(lastMilliseconds : Long) = {
+    messageProvider.getMessages(lastMilliseconds)
+  }
+
+}
+
+object ChatEvaluation {
+
+  def apply(messageProvider: YouTubeMessageProvider): ChatEvaluation = new ChatEvaluation(messageProvider)
 
 }
